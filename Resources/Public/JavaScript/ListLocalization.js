3656b7e75b22da89a6f1f046331b605328bc2393
Martin Neumann <43402899+mnediw@users.noreply.github.com>
2024-06-10 17:28:09 +0200

    Update ListLocalization.js
    Link in list mode was the same for all languages. This fixes the links if more than 1 localization lagunage is available.

--- Resources/Public/JavaScript/ListLocalization.js
+++ Resources/Public/JavaScript/ListLocalization.js
@@ -2,7 +2,10 @@ define('TYPO3/CMS/DdDeepl/ListLocalization', ['jquery'], function ($) {
   $('.recordlist[id^="t3-table-tx_"] .col-localizationb .btn-group').each(function() {
     let that = $(this);
     let newElement = that.clone().insertAfter(that);
-    let link = newElement.find('a.t3js-action-localize');
+    newElement.find('a.t3js-action-localize').each(function() {
+      let link = $(this);
+      link.attr('href', link.attr('href') + '&deepl=1');
+    });
     link.attr('href', link.attr('href') + '&deepl=1');
     let img = newElement.find('img');
     $('<span class="deepl-icon-overlay"></span>').insertAfter(img.parent().parent());
